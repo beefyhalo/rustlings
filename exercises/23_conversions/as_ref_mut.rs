1b63174fec89433978dae4f9ac66c555224099f6
Kevin Horlick <KevinHorlick@gmail.com>
2025-04-28 19:25:00 -0400

    optimize

--- exercises/23_conversions/as_ref_mut.rs
+++ exercises/23_conversions/as_ref_mut.rs
@@ -18,7 +18,8 @@ fn char_counter<T: AsRef<str>>(arg: T) -> usize {
 // Squares a number using `as_mut()`.
 // TODO: Add the appropriate trait bound.
 fn num_sq<T: AsMut<u32>>(arg: &mut T) {
-    *arg.as_mut() *= *arg.as_mut()
+    let arg = arg.as_mut();
+    *arg *= *arg;
 }
 
 fn main() {
